b292589ea6d4560e810512f04107550ab32211ac
arinja <sorokhaibanarinja@gmail.com>
2026-03-21 17:37:35 +0530

    quest_7_solved

---
+++ Assessment_2_/Question_7.js
@@ -0,0 +1,8 @@
+// let c =['hey','keyboard','mouse','kim']
+// filter out words which have 'ke'in them
+
+let c = ['hey', 'keyboard', 'mouse', 'kim'];
+
+let result = c.filter(x => x.includes('ke'));
+
+console.log(result);
