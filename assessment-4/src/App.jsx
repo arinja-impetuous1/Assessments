74ec1c7fef3367e7f50e323f4e7392bed34b5375
arinja <sorokhaibanarinja@gmail.com>
2026-05-06 01:22:44 +0530

    return

--- assessment-4/src/App.jsx
+++ assessment-4/src/App.jsx
@@ -26,4 +26,24 @@ const App = () => {
     const newTasks = tasks.filter((_, i) => i !== index);
     setTasks(newTasks);
   };
-}
+
+   return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Task Manager</h1>
+
+      <input
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        placeholder="Enter a task"
+      />
+      <button onClick={addTask}>Add</button>
+
+      <hr />
+
+      <TaskList tasks={tasks} deleteTask={deleteTask} />
+    </div>
+  );
+};
+
+export default App;
