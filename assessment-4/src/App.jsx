6b9216041d08eed181447c5864792c8180312768
arinja <sorokhaibanarinja@gmail.com>
2026-05-06 01:18:37 +0530

    useEffect loaded data

--- assessment-4/src/App.jsx
+++ assessment-4/src/App.jsx
@@ -1,7 +1,14 @@
 import React, { useState, useEffect } from 'react'
 
 const App = () => {
-  
+
     const [tasks, setTasks] = useState([]);
   const [input, setInput] = useState('');
+
+    useEffect(() => {
+    const savedTasks = JSON.parse(localStorage.getItem('tasks'));
+    if (savedTasks) {
+      setTasks(savedTasks);
+    }
+  }, []);
 }
